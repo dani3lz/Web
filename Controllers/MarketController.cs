c2eb03bf95307984cd610e521b4ee0934c81d6db
dani3lz <69303264+dani3lz@users.noreply.github.com>
2021-02-20 17:42:56 +0200

    add folder

---
+++ Controllers/MarketController.cs
@@ -0,0 +1,21 @@
+﻿using eUseControl.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eUseControl.Web.Controllers
+{
+    public class MarketController : Controller
+    {
+          // GET: Market
+          public ActionResult Main()
+          {
+               UserData u = new UserData();
+               u.Category = new List<string> { "TOP", "Drama", "Fantastic", "Biografie", "RO", "RU", "EN", "Audio" };
+               u.Books = new List<string> { "Cartea #1", "Cartea #2", "Cartea #3", "Cartea #4", "Cartea #5", "Cartea #6", "Cartea #7" };
+               return View(u);
+          }
+    }
+}
---
+++ Models/UserData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eUseControl.Web.Models
+{
+     public class UserData
+     {
+          public List<String> Category { get; set; }
+          public List<String> Books { get; set; }
+     }
+}
