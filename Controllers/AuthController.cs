4a7e4f8dfefa5270af3e92c9731ffa38bfa3f6a1
dani3lz <69303264+dani3lz@users.noreply.github.com>
2021-02-20 21:02:08 +0200

    add register and scripts

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -13,5 +13,9 @@ namespace eUseControl.Web.Controllers
         {
             return View();
         }
-    }
+          public ActionResult Register()
+          {
+               return View();
+          }
+     }
 }
